826e36f882a1458c8100e71f5c18521ecdf3a993
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-09-02 11:14:18 +0530

    added vigenere cipher file added

--- vigener_decrypt.cpp
+++ vigener_decrypt.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-void vigenere_cipher(string, string); // function prototyping
+void vigenere_decrypt(string, string); // function prototyping
 string generate_key(string, string);  // function prototyping
 
 int main(void)
 {
-    cout << "enter the message without including space in between them and must be in small letters " << endl;
+    cout << "enter the message without including space in between them " << endl;
     string plaintext;
     getline(cin, plaintext);
 
@@ -16,27 +16,30 @@ int main(void)
     string key;
     getline(cin, key);
 
-    vigenere_cipher(plaintext, key);
+    vigenere_decrypt(plaintext, key);
 
     return 0;
 }
 
-void vigenere_cipher(string plaintext, string key)
+void vigenere_decrypt(string plaintext, string key)
 {
     string new_key = generate_key(plaintext, key);
-    string cipher_text = "";
+    string decrypt_text ="";
 
-    for (int i = 0; i < plaintext.size(); i++)
+    for (int i = 0; i < plaintext.size() ; i++)
     {
-        cipher_text.push_back(char(int((plaintext[i] - 97) + (new_key[i] - 97)) % 26) + 97); // 97 is done to converrt into small letters
+        decrypt_text.push_back(char ((plaintext[i] + new_key[i]) % 26) + 97 ); // 97 is done to converrt into small letters 
     }
 
-    cout << "the encrypted text is " << endl;
 
-    for (int i = 0; i < cipher_text.size(); i++)
+    cout <<"the decrypted text is "<<endl;
+
+    for ( int i = 0 ; i < decrypt_text.size() ; i++)
     {
-        cout << cipher_text[i];
+        cout <<decrypt_text[i];
     }
+
+    
 }
 
 string generate_key(string plaintext, string key)
@@ -44,14 +47,15 @@ string generate_key(string plaintext, string key)
     int size_of_plaintext = plaintext.size() - key.size();
     int j = 0;
 
+
     for (int i = 0; i < size_of_plaintext; i++)
     {
-
+        
         if (key.size() == plaintext.size())
         {
             break; // key length and plaintext(message) length is same
         }
-
+        
         else
         {
             key.push_back(key[j]);
@@ -63,6 +67,6 @@ string generate_key(string plaintext, string key)
             j = 0;
         }
     }
-
+    
     return key;
 }
