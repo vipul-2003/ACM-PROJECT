daa0f4a62de3c49dc9fb51710b29207006ea5a20
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-09-02 12:11:38 +0530

    added vigenere decrypt file

--- vigener_decrypt.cpp
+++ vigener_decrypt.cpp
@@ -9,28 +9,28 @@ string generate_key(string, string);  // function prototyping
 int main(void)
 {
     cout << "enter the message without including space in between them " << endl;
-    string plaintext;
-    getline(cin, plaintext);
+    string ciphertext;
+    getline(cin, ciphertext);
 
     cout << "enter the key " << endl;
     string key;
     getline(cin, key);
 
-    vigenere_decrypt(plaintext, key);
+    vigenere_decrypt(ciphertext, key);
 
     return 0;
 }
 
-void vigenere_decrypt(string plaintext, string key)
+void vigenere_decrypt(string ciphertext, string key)
 {
-    string new_key = generate_key(plaintext, key);
+    string new_key = generate_key(ciphertext, key);
     string decrypt_text ="";
 
-    for (int i = 0; i < plaintext.size() ; i++)
+    for (int i = 0; i < ciphertext.size() ; i++)
     {
-        decrypt_text.push_back(char ((plaintext[i] + new_key[i]) % 26) + 97 ); // 97 is done to converrt into small letters 
+        decrypt_text.push_back(char (int ((ciphertext[i] -97) - (new_key[i] - 97)  ) % 26) + 97 ); // 97 is done to converrt into small letters 
     }
-
+ 
 
     cout <<"the decrypted text is "<<endl;
 
@@ -42,21 +42,32 @@ void vigenere_decrypt(string plaintext, string key)
     
 }
 
-string generate_key(string plaintext, string key)
+/*
+
+this will generate the key of same length of cipher text  
+
+*/
+
+string generate_key(string ciphertext, string key)
 {
-    int size_of_plaintext = plaintext.size() - key.size();
+    int size_of_ciphertext = ciphertext.size() - key.size();
     int j = 0;
 
 
-    for (int i = 0; i < size_of_plaintext; i++)
+    for (int i = 0; i < size_of_ciphertext; i++)
     {
         
-        if (key.size() == plaintext.size())
+        if (key.size() == ciphertext.size())
         {
-            break; // key length and plaintext(message) length is same
+            break; // key length and ciphertext(message) length is same
         }
+        /*
         
-        else
+        this is used to make the keylength equal to ciphertext length 
+        by repeating itself again and again upto cipher text length  
+        
+        */
+        else  
         {
             key.push_back(key[j]);
             j++;
